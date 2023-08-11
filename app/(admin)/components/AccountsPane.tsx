d2fe2a272045dec741d9667e27488e90e9b3d4d5
Ifeanyi Isitor <ifeanyi.isitor@flute.io>
2023-08-11 10:46:56 +0100

    Incorporate talwind-styled-components

--- app/(admin)/components/AccountsPane.tsx
+++ app/(admin)/components/AccountsPane.tsx
@@ -1,18 +1,22 @@
+import tw from 'tailwind-styled-components'
 import { Button } from '@/app/components/ui/button'
 import { AccountsTable } from './AccountsTable'
+
+const Header = tw.header`flex justify-between items-center border-b p-8 pl-10 pr-10`
+const Heading = tw.div`flex flex-col gap-2`
+const HeadingTitle = tw.h2`font-medium tracking-tighter text-5xl`
+const HeadingSubtitle = tw.p`text-md text-muted-foreground font-light tracking-tight max-w-md leading-normal`
 
 export function AccountsPane() {
   return (
     <>
-      <header className="flex justify-between items-center border-b p-8 pl-10 pr-10">
-        <div>
-          <h2 className="font-medium tracking-tighter text-5xl">Accounts</h2>
-          <p className="text-md text-muted-foreground font-light tracking-tight max-w-md leading-normal mt-2">
-            View and manage your account
-          </p>
-        </div>
+      <Header>
+        <Heading>
+          <HeadingTitle>Accounts</HeadingTitle>
+          <HeadingSubtitle>View and manage your account</HeadingSubtitle>
+        </Heading>
         <Button className="font-normal">Add Account</Button>
-      </header>
+      </Header>
       <AccountsTable />
     </>
   )
